bee4e462aecc85c2b67335819e5788e2202b2b3d
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-22 19:23:14 +0000

    endorements.js

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -1,23 +1,21 @@
 require "rails_helper"
 
 feature "endorsing reviews" do
-  before { xavier_signs_up ; add_nandos }
+  before { xavier_signs_up ; add_nandos ; add_review_first }
 
   scenario "displays the number of endorsements per review" do
-    add_review_first
     expect(page).to have_content "0 endorsements"
   end
 
   scenario "a user can endorse a review, which updates the review endorsement count" do
     visit "/restaurants"
-    add_review_first
     click_link "Endorse review"
     expect(page).to have_content("1 endorsement")
   end
 
   it "a user can endorse a review, which increments the endorsement count", js: true do
     visit "/restaurants"
-    click_link "Endorse"
+    click_link "Endorse review"
     expect(page).to have_content("1 endorsement")
   end
 end
