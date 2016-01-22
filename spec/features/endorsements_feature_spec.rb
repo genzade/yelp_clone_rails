990772b3a4da0b510cbb494fc416323382395cc6
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-22 19:04:50 +0000

    phantomJS endorements

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -8,10 +8,16 @@ feature "endorsing reviews" do
     expect(page).to have_content "0 endorsements"
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
-    visit '/restaurants'
+  scenario "a user can endorse a review, which updates the review endorsement count" do
+    visit "/restaurants"
     add_review_first
-    click_link 'Endorse review'
-    expect(page).to have_content('1 endorsement')
+    click_link "Endorse review"
+    expect(page).to have_content("1 endorsement")
+  end
+
+  it "a user can endorse a review, which increments the endorsement count", js: true do
+    visit "/restaurants"
+    click_link "Endorse"
+    expect(page).to have_content("1 endorsement")
   end
 end
